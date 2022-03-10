2eab53b9be4c2a37bde16a3fb8474f109faa3b3f
Nathan Wong <93850704+nk-wong@users.noreply.github.com>
2022-03-10 15:15:31 -0500

    Added templates and fields to player object

---
+++ UniTale/Assets/Scripts/Character.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    int health; //Player health
+    int nutrition; //Score to monitor food consumption
+    int hygeine; //Score to monitor cleanliness
+    int intelligence; //Score to monitor educational performance
+    int social; //Score to monitor friendliness
+    int energy; //Score to monitor physical performance
+    int sanity; //Score to monitor mental performance
+    int addiction; //Score to monitor habits
+    double money; //Tracks the amount of money the player has
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+    //Sleep to increase health
+    void Sleep()
+    {
+
+    }
+
+    //Eat to either increase or decrease nutrition based on the food consumed
+    void Eat()
+    {
+
+    }
+
+    //Clean to increase hygeine
+    void Clean()
+    {
+
+    }
+
+    //Study to increase intelligence
+    void Study()
+    {
+
+    }
+
+    //Exercise to increase social
+    void Exercise()
+    {
+
+    }
+}
